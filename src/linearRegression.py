908a03f5f95e0cd29a56847750e64b2da3ac3db7
Frank Lee <franklee@umail.ucsb.edu>
2019-07-24 22:55:13 -0700

    First commit

---
+++ src/linearRegression.py
@@ -0,0 +1,14 @@
+# linear regression
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+ocedTable = pd.read_csv("../data/oecd.csv", thousands=',')
+gdpTable = pd.read_csv("../data/gdp.csv", thousands=",", delimiter="\t", encoding="latin1",
+                       na_values='n/a')
+print(ocedTable[ocedTable["INEQUALITY"] == "TOT"])
+
+
+def prepareCountryStats(ocedTable):
+    return ocedTable[ocedTable["INEQUALITY" == "TOT"]]
